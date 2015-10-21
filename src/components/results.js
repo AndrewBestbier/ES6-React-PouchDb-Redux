288969423b7ee064c6ac2793bfc1685ad58c5678
Andrew Bestbier <andrew.bestbier@gmail.com>
2015-10-21 10:09:59 +0200

    Added charts dependencies and an example chart to get going

--- src/components/results.js
+++ src/components/results.js
@@ -2,12 +2,37 @@
 
 import React from 'react';
 
+import { Pie } from 'react-chartjs';
+
 export default React.createClass({
 
   render() {
+
+    var data = [
+    {
+        value: 300,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+    },
+    {
+        value: 50,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Green"
+    },
+    {
+        value: 100,
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "Yellow"
+    }
+]
+
     return (
       <div>
         <h3>Results</h3>
+        <Pie data={data}/>
       </div>
     );
   }
